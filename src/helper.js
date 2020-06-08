70fce14f750e17fde2cfbb8509cb4e5ed3c0aac6
Michael Salim <Vija02@users.noreply.github.com>
2020-06-08 11:43:51 +0100

    refacor: update how we treat the url to allow our code to me more generic

--- src/helper.js
+++ src/helper.js
@@ -8,7 +8,7 @@ exports.error = (...params) => console.error(chalk.red(...params))
 
 const getPlayParams = (videoId = null, method = 'REPLACE_ALL', previousToken = null) => {
 	const id = videoId || state.videoId
-	return [method, `${process.env.YOUTUBE_STREAMER_URL}/cache/${id}`, id, 0, previousToken]
+	return [method, `${process.env.YOUTUBE_STREAMER_URL}/${id}`, id, 0, previousToken]
 }
 exports.getPlayParams = getPlayParams
 
